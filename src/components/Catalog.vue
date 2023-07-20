94a96d2f91183bb301cb6a4b9a122c4bfc19b463
Denis <denis.gordievsky@1win.pro>
2023-07-20 15:11:41 +0700

    add change images in Catalog

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -2,39 +2,17 @@
 
   <ul class="catalog__list">
 
-    <li class="catalog__item" v-for="product in products" :key="product.id">
-      <router-link :to="{name: 'product', params: {productId: product.id}}" class="catalog__pic">
-        <img :src="product.colors[0].gallery ? product.colors[0].gallery[0].file.url : 'images/no-img.png'" :alt="product.colors[0].gallery ? product.title : 'Изображение товара отсутствует'">
-      </router-link>
-
-      <h3 class="catalog__title">
-        <router-link  :to="{name: 'product', params: {productId: product.id}}">
-          {{ product.title }}
-        </router-link>
-      </h3>
-
-      <span class="catalog__price">
-            {{ product.price }} ₽
-          </span>
-
-      <ul class="colors colors--black">
-
-        <li class="colors__item" v-for="color in product.colors" :key="color.id">
-          <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" :name="'color'  + product.id" :value="color.color.code">
-            <span class="colors__value" :style="{ backgroundColor: color.color.code }"></span>
-          </label>
-        </li>
-
-      </ul>
-    </li>
+    <CatalogProduct v-for="product in products" :key="product.id" :colors="product.colors" :price="product.price" :id="product.id" :title="product.title"/>
 
   </ul>
 
 </template>
 
 <script>
+import CatalogProduct from "@/components/CatalogProduct.vue";
+
 export default {
+  components: {CatalogProduct},
   props: ['products'],
 }
 </script>
---
+++ src/components/ColorInput.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <li class="colors__item">
+    <label class="colors__label">
+      <input class="colors__radio sr-only" type="radio" name="color" :value="color.color.code" @click="$emit('changeCurrentImage', color.id)">
+      <span class="colors__value" :style="{ backgroundColor: color.color.code }"></span>
+    </label>
+  </li>
+
+</template>
+
+<script>
+export default {
+  props: ['color'],
+}
+</script>
